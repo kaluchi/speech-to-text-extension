b2f7081a67a760541048126102e044981268878a
Евгений Калитько <kaluchi@gmail.com>
2025-03-21 06:13:50 +0300

    Обновление manifest.json
    Добавлены новые файлы сервисов:
    - api-request-builder.js
    - recorder.js
    - keyboard.js
    
    Заменены старые имена файлов для соответствия новой структуре

--- services/keyboard-controller.js
+++
@@ -1,8 +0,0 @@
-/**
- * Для обратной совместимости.
- * @deprecated Используйте keyboard.js вместо этого файла
- */
-console.warn('keyboard-controller.js устарел и будет удален в следующих версиях. Используйте keyboard.js');
-
-// Перенаправляем на новый класс
-window.PageObjectKeyboardControllerService = window.PageObjectKeyboardService;
--- services/recording-controller.js
+++
@@ -1,8 +0,0 @@
-/**
- * Для обратной совместимости.
- * @deprecated Используйте recorder.js вместо этого файла
- */
-console.warn('recording-controller.js устарел и будет удален в следующих версиях. Используйте recorder.js');
-
-// Перенаправляем на новый класс
-window.PageObjectRecordingControllerService = window.PageObjectRecorderService;
